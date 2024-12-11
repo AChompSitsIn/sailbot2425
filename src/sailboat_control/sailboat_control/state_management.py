41ad3b13c303ed1422cc3648744575948e4468a2
AChompSitsIn <114684575+AChompSitsIn@users.noreply.github.com>
2024-12-11 14:04:18 -0800

    updated class names, improved documentation, added TODO list

--- src/sailboat_control/sailboat_control/state_management.py
+++ src/sailboat_control/sailboat_control/state_management.py
@@ -1,3 +1,26 @@
+# ---------------- TO-DO LIST ---------------- 
+# Add the predefined waypoints within each event class
+# Implement the control() functionality
+    # In tandem with this, evaluate how to handle RC control/interruptions
+    # For full RC events (fleet race) I plan on making a second RC only control method to reduce the autonomous control method's complexity
+    # For autonomous events with potential RC interrupts, the current plan will be to:
+        # watch for incoming RC commands
+        # upon receiving an RC command, control should return the most recently passed waypoint and forfeit control to RC commands
+        # we will continue to use autonomous distance checks for passed buoys but also allow for manual intervention to mark a passed buoy
+        # upon receiving a command to restart autonomous control, the boat will recalculate its path from its current position
+
+        # This approach simplifies the ROS node structure at the cost of reducing the modularity of the control functions 
+# Create a 'waypoint' class, which contains a waypoint's latitude, longitude, and a passed flag. passed will be set to true when the boat has passed it
+    # This should allow for better management of which leg the boat is currently on
+    # I expect to treat endurance as 16 individual waypoints, as opposed to 4 waypoints looped 4 times, as an attempt to improve course leg clarity and visualization
+
+# The logic flow should be as follows:
+    # boat object is defined with an event type -> waypoints are loaded into control -> boat is initially in RC control and awaits a signal to start autonomous ->
+    # autonomous signal is received -> boat uses its current position to navigate to the first target and so on
+
+    # in the event that the wind angle changes, we can re-call leg with the new wind to accommodate
+
+
 class Boat:
     def __init__(self, event_type: str):
 
@@ -8,20 +31,22 @@ class Boat:
         """
 
         self.event_type = event_type
-        self.autonomous_system_initialize = False
+        self.autonomous_system_initialize = False # we will need to modify this flag, planning on using a fundamental control() method which takes the event type and waypoints as parameters
 
     def pick_event(self):
         """
         Pick and initialize the event based on the event type
         """
 
+        # Used a dictionary to make the setting of event types more obvious
         event_classes = {
 
-            "fleet_race": FleetRace,
-            "precision_navigation": PrecisionNavigation,
-            "station_keeping": StationKeeping,
-            "endurance": Endurance,
-            "payload": Payload
+            "fleet_race": F,
+            "precision_navigation": Pr,
+            "station_keeping": S,
+            "endurance": E,
+            "payload": P,
+            "developer_mode": D
 
         }
 
@@ -45,7 +70,7 @@ class Event:
 
         raise NotImplementedError("This method should be implemented in subclasses")
 
-class FleetRace(Event):
+class F(Event):
     
     def initialize_event(self, boat: Boat):
         """
@@ -54,46 +79,55 @@ class FleetRace(Event):
 
         print("Initializing Fleet Race Event Type...")
 
-class PrecisionNavigation(Event):
+class Pr(Event):
 
     def initialize_event(self, boat: Boat):
         """
         Initialize the precision navigation event      
         """
 
-        print("Initializing The Precision Navigation Event Type")
+        print("Initializing The Precision Navigation Event Type...")
         boat.autonomous_system_initialized = True
         print("Autonomous System Initialized For Precision Navigation")
 
-class StationKeeping(Event):
+class S(Event):
 
     def initialize_event(self, boat: Boat):
         """
-        Initialize the station keeping event
+        Initialize the station-keeping event
         """
 
-        print("Initializing The Station Keeping Event Type")
+        print("Initializing The Station Keeping Event Type...")
         boat.autonomous_system_initializef = True
         print("Autonomous System Initialized For Station Keeping")
 
-class Endurance(Event):
+class E(Event):
 
     def initialize_event(self, boat: Boat):
         """
         Initialize the endurance event
         """
 
-        print("Initializing The Endurance Event Type")
+        print("Initializing The Endurance Event Type...")
         boat.autonomous_system_initializef = True
         print("Autonomous System Initialized For Endurance")
 
-class Payload(Event):
+class P(Event):
 
     def initialize_event(self, boat: Boat):
         """
         Initialize the payload event
         """
 
-        print("Initializing The Payload Event Type")
+        print("Initializing The Payload Event Type...")
         boat.autonomous_system_initializef = True
         print("Autonomous System Initialized For Payload")
+
+class D(Event):
+
+    def initialize_event(self, boat: Boat):
+        """
+        Initialize developer mode
+        """
+
+        print("Initializing Developer Mode...")
